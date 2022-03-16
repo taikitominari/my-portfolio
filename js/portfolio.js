61bfc23bfc188992e17cd692082b492d1791197f
taikitominari <taikitominari.co.jp@icloud.com>
2022-03-16 21:56:29 +0900

    各セクションの雛形を作成

---
+++ js/portfolio.js
@@ -0,0 +1,13 @@
+'use strict';
+// なめらかにスクロール
+$(function(){
+  $('a[href^="#"]').click(function(){
+    var adjust = 0;
+    var speed = 800;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top + adjust;
+    $('body,html').animate({scrollTop:position}, speed, 'swing');
+    return false;
+  });
+});
