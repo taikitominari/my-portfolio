46eea825fa5e17a7ed566ada678789112312ef7d
taikitominari <taikitominari.co.jp@icloud.com>
2022-03-18 00:51:59 +0900

    main_logoのフェードイン実装 profileのフェードイン変更

--- js/portfolio.js
+++ js/portfolio.js
@@ -12,18 +12,59 @@ $(function(){
   });
 });
 
+// main_logoのフェードイン
+$(function(){
+  $('.main_logo').children().addBack().contents().each(function() {
+      $(this).replaceWith($(this).text().replace(/(\S)/g, '<span class="text-move">$&</span>'));
+  });
+   setTimeout(function(){
+      $(".main_logo").addClass("active");
+  },100);
+});
+
+
+// .logoのフェードイン
+let container = $(".logo");
+let speed = 200;
+let content = container.html();
+let text = $.trim(content);
+let newHtml = "";
+
+text.split("").forEach(function(v) {
+ newHtml += '<span>' + v + '</span>';
+});
+
+container.html(newHtml);
 
 $(function(){
   $(window).on('scroll', function() {
       if($(this).scrollTop() + $(window).height() > $('.profile_item').offset().top){
-        $('.profile_item').addClass('active');
+        let txtNum = 0;
+        container.css({opacity: 1});
+        setInterval(function() {
+        container.find('span').eq(txtNum).css({opacity: 1});
+        txtNum++
+        }, speed);
       }
-  });
+    });
 });
+
+
 $(function(){
   $(window).on('scroll', function() {
       if($(this).scrollTop() + $(window).height() > $('.profile_item').offset().top){
-        $('.profile_logo').addClass('show');
+        let i = 0;
+        setInterval(function() {
+          $('.profile_item').eq(i).addClass('item_active');
+          i++;
+          if(i>=$('.profile_item').length) i=0;
+        },400)
       }
-  });
+    });
 });
+
+
+
+
+
+
